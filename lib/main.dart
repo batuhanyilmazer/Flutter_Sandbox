d3187e641aa887e55bde0e23613f2250612aac2c
batuhanyilmazer <batuhanayilmazer@gmail.com>
2023-02-17 20:22:08 +0300

    Add button and alertDialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  String mesaj = "Başlık";
+  runApp(MaterialApp(
+    home: MyApp()
+  ));
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar Text"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              var alert = AlertDialog(
+                title: Text("AlertDialog Başlık"),
+                content: Text("AlertDialog Metin"),
+              );
+              showDialog(context: context, builder: (BuildContext context)=>alert);
+            },
+            child: Text("Buton"),
+          )
+      ),
+    );
+  }
 }
